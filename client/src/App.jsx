6c51675d8453f32ff760f7b8750941b6d41ced6f
om7057 <kulkarniom7057@gmail.com>
2025-11-30 18:50:33 +0530

    basic client setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-xl font-family-main">
+      <h1>Welcome to the App!</h1>
+    </div>
+  );
+};
+
+export default App;
